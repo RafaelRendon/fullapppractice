ca9822a3d1c5c96323d5f957865412d1daa5e48a
DaXRated <rantoni31@gmail.com>
2024-01-18 21:15:51 -0600

    primer commit, solo api creada

---
+++ api/src/models/index.js
@@ -0,0 +1,16 @@
+const {Schema, model} = require("mongoose");
+
+const noteSchema = new Schema(
+    {
+     status: Boolean,
+     task: String,
+    },
+    {
+     timestamps: false,
+     versionKey: false
+    }
+);
+
+const Task = new model("Notes", noteSchema)
+
+module.exports = Task;
---
+++ api/src/routers/index.js
@@ -0,0 +1,11 @@
+const { Router } = require('expresss');
+
+const route = Router();
+
+route.get("/", (req, res)=>{});
+
+route.post("/", (req, res)=>{});
+
+route.put("/", (req, res)=>{});
+
+route.delete("/", (req, res)=>{});
